9a1b86d60aad66073ced765705e78646cd909756
lê thăng long <long2323123@gmail.com>
2021-11-03 12:51:25 +0700

    update

---
+++ main.js
@@ -0,0 +1,20 @@
+// getElementById
+// getElementBuyClassName
+// getElemenBuyTagName
+// querySelector
+// quẻySelecttorAll
+
+// HTML collection
+
+// doccument.write
+
+// inneHTML = ghi đè lên thg con trong thẻ đc gọi ra
+// outerHTML = ghi đè lên chính thẻ đc gọi ra
+
+
+// var boxElement = document.querySelector('.box');
+
+// boxElement.innerHTML = '<h1> heading </h1>'
+
+// console.log(boxElement);
+
